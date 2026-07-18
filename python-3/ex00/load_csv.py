a4a902263e458327b4dd5a167d4cd6cd90d11a35
medmedhaddioui <haddiouimedmed@gmail.com>
2026-07-18 18:49:44 +0100

    update

---
+++ python-3/ex00/load_csv.py
@@ -0,0 +1,11 @@
+def findDisappearedNumbers( nums) :
+        nums.sort() 
+        print( nums)
+        missing_elements =  []
+        for index , i  in enumerate(nums):
+            if i != index+ 1:
+                print(i , index)
+                missing_elements.append(i)
+        return missing_elements
+test = [4,3,2,7,8,2,3,1]
+print(findDisappearedNumbers(test))
