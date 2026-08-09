bc1550827bc216990ce4529f4c4f1b089bac06e3
medmedhaddioui <haddiouimedmed@gmail.com>
2026-08-09 18:10:13 +0100

    complete ex03

--- python-4/ex03/ft_calculator.py
+++ python-4/ex03/ft_calculator.py
@@ -1,29 +1,35 @@
 class calculator:
-#your code here
-    def __init__ (self, list: list):
+    """A class to perform vector operations (add, mul, sub, truediv)."""
+
+    def __init__(self, list: list):
+        """Initialize calculator with a list of numbers."""
         self.vector = list
 
     def __add__(self, object) -> None:
-        for index, i in enumerate (self.vector):
+        """Add a scalar to each element in vector."""
+        for index, i in enumerate(self.vector):
             self.vector[index] = i + object
         print(self.vector)
 
-
     def __mul__(self, object) -> None:
-        for index, i in enumerate (self.vector):
+        """Multiply each element in vector by a scalar."""
+        for index, i in enumerate(self.vector):
             self.vector[index] = i * object
         print(self.vector)
-    
-    def __sub__(self, object) -> None:
 
-        for index, i in enumerate (self.vector):
-                self.vector[index] = i - object
+    def __sub__(self, object) -> None:
+        """Subtract a scalar from each element in vector."""
+        for index, i in enumerate(self.vector):
+            self.vector[index] = i - object
         print(self.vector)
 
     def __truediv__(self, object) -> None:
-        for index, i in enumerate (self.vector):
-            if (i == 0 and object == 0):
-                print("impossible")
-                return 
-            self.vector[index] = i / object
-        print(self.vector)
+        """Divide each element in vector by a scalar."""
+        try:
+            for index, i in enumerate(self.vector):
+                if object == 0:
+                    raise ZeroDivisionError("Division by zero not allowed.")
+                self.vector[index] = i / object
+            print(self.vector)
+        except ZeroDivisionError as e:
+            print("ZeroDivisionError:", e)
